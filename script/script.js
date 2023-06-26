e5f2142ab6c646e57d752358f26d8d9ce6d26d5d
prashanta <pbt9840@gmail.com>
2023-06-26 11:11:17 +0545

    feat:project section added

---
+++ script/script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".slider-content", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    grabCursor:'true', 
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets:true,  //dyanamic bullet pagination
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
